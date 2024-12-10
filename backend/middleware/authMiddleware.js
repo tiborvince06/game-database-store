8ac13f1122328bf2b2dcca8fa0fbfce02bbf9099
tiborvince06 <tiborvince06@gmail.com>
2024-12-10 17:53:16 +0100

    feat: creating middleware for verifycation

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,27 @@
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+
+async function verifyToken(req, res, next) {
+    const token = req.header('Authorization')?.split(' ')[1];
+
+    if (!token) {
+        return res.status(401).json({ error: 'Access denied. No token provided.' });
+    }
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.userId = decoded.userId;
+
+        const user = await User.findById(req.userId);
+        if (!user) {
+            return res.status(404).json({ error: 'User not found' });
+        }
+
+        req.user = user;
+        next();
+    } catch (error) {
+        res.status(401).json({ error: 'Invalid or expired token' });
+    }
+}
+
+module.exports = verifyToken;
