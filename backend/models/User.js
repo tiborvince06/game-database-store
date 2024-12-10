3653161542229e6efa8be34d8080ca3e3d291ffb
tiborvince06 <tiborvince06@gmail.com>
2024-12-10 17:55:01 +0100

    fix: User database change

--- backend/models/User.js
+++ backend/models/User.js
@@ -4,12 +4,8 @@ const userSchema = new mongoose.Schema(
   {
     name: { type: String },
     email: { type: String, unique: true },
-    emailVerified: { type: Date },
-    image: { type: String },
     password: { type: String },
     role: { type: String, enum: ["user", "admin"], default: "user" },
-    createdAt: { type: Date, default: Date.now },
-    updatedAt: { type: Date, default: Date.now },
     likedGames: [
       {
         type: mongoose.Schema.Types.ObjectId,
